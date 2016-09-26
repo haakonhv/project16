3d505fe8c53b3394d823057cad1c44f4f38ab622
hoeiland <haakoho@stud.ntnu.no>
2016-09-26 14:03:22 +0200

    Fikset felt

--- src/project16/Team.java
+++ src/project16/Team.java
@@ -1,5 +1,18 @@
 package project16;
 
 public class Team {
-
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
