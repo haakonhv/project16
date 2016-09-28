8348d30c05233e220cda640c7e10728919e3af6b
hoeiland <haakoho@stud.ntnu.no>
2016-09-28 12:45:18 +0200

    Lagt til atributter i Game og Player

--- src/project16/Player.java
+++ src/project16/Player.java
@@ -3,6 +3,51 @@ package project16;
 public class Player {
 
 	int id;
+	String first_name;
+	String last_name;
+	String real_position;
+	String country;
+	int birth_year;
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getReal_position() {
+		return real_position;
+	}
+
+	public void setReal_position(String real_position) {
+		this.real_position = real_position;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getBirth_year() {
+		return birth_year;
+	}
+
+	public void setBirth_year(int birth_year) {
+		this.birth_year = birth_year;
+	}
 
 	public int getId() {
 		return id;
