311ce6eb2d3dd4bcb8d7962b2d6d263b25213bab
haakonhv <haakosh@stud.ntnu.no>
2016-09-28 14:21:53 +0200

    Kan sende Qualifiers

--- src/project16/Main.java
+++ src/project16/Main.java
@@ -10,13 +10,12 @@ import org.xml.sax.SAXException;
 
 import java.sql.*;
 
-public class Main {
+public class Main{
 
-	public static String xmlGameFile;
+	public static String xmlGameFile = "f24-90-2014-739631-eventdetails.xml";;
 
-	public static void sendGame() throws ParserConfigurationException, SAXException, IOException{
-		xmlGameFile = "f24-90-2014-739631-eventdetails.xml";
-//		MyDomParser.getDocument(xmlGameFile);
+
+	public static void sendGame() throws ParserConfigurationException, SAXException, IOException, SQLException{
 		Document doc = MyDomParser.getDocument(xmlGameFile);
 		Game game = MyDomParser.getGame(doc);
 		ArrayList<Event> eventList = MyDomParser.getEventList(doc, game);
@@ -28,8 +27,50 @@ public class Main {
 
 
 	}
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
-		sendGame();
+
+	public static void sendEvents() throws ParserConfigurationException, SAXException, IOException, SQLException{
+		Document doc = MyDomParser.getDocument(xmlGameFile);
+		Game game = MyDomParser.getGame(doc);
+		ArrayList<Event> eventList = MyDomParser.getEventList(doc, game);
+		MyDomParser parser = new MyDomParser(eventList, doc, game);
+		for(int i=0; i<eventList.size();i++){
+			String eventID =  Integer.toString(parser.eventList.get(i).id);
+			String typeID =  Integer.toString(parser.eventList.get(i).value);
+			String teamID =  Integer.toString(parser.eventList.get(i).teamid);
+			String playerID =  Integer.toString(parser.eventList.get(i).playerid);
+			String xStart =  Float.toString(parser.eventList.get(i).xstart);
+			String yStart =  Float.toString(parser.eventList.get(i).ystart);
+			String gameID =  Float.toString(parser.game.id);
+			String values = eventID+","+typeID+","+xStart+","+yStart+","+gameID;
+			DataBaseConnector.insert("EVENT", values);
+
+		}
+
+	}
+
+	public static void sendQualifiers() throws ParserConfigurationException, SAXException, IOException, SQLException{
+		Document doc = MyDomParser.getDocument(xmlGameFile);
+		Game game = MyDomParser.getGame(doc);
+		ArrayList<Event> eventList = MyDomParser.getEventList(doc, game);
+		MyDomParser parser = new MyDomParser(eventList, doc, game);
+		for(int i=0; i<eventList.size();i++){
+			for (int j=0; j<eventList.get(i).getQualifierList().size();j++){
+				Qualifier thisQual = parser.eventList.get(i).getQualifierList().get(j);
+				String qID = Integer.toString(thisQual.id);
+				String qualifierID = Integer.toString(thisQual.qualifier_id);
+				String eventID = Integer.toString(parser.eventList.get(i).id);
+				String values = qID+","+qualifierID+","+eventID;
+				DataBaseConnector.insert("QUALIFIER", values);
+
+			}
+		}
+
+	}
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, SQLException, ClassNotFoundException{
+		DataBaseConnector.openConnection();
+		sendEvents();
+		sendQualifiers();
+		DataBaseConnector.closeConnection();
 	}
 
 
