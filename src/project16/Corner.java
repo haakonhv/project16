51836eb85864dac70da907c0be225b1a5332ba86
hoeiland <haakoho@stud.ntnu.no>
2016-10-04 15:32:13 +0200

    corner

---
+++ src/project16/Corner.java
@@ -0,0 +1,11 @@
+package project16;
+
+public class Corner {
+	int corner_id;
+	String ballbane;
+	float koord_x;
+	float koord_y;
+	int first_post; //binary
+	int second_pos; //binary
+	int event_id; //foreign key
+}
