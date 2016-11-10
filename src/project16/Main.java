9e4fb97a8d03b53ca01309f67f4361a4d43c4f14
hoeiland <haakoho@stud.ntnu.no>
2016-11-10 14:58:26 +0100

    legger inn def id

--- src/project16/Main.java
+++ src/project16/Main.java
@@ -401,15 +401,16 @@ public class Main{
 				if (cornerTeamID==homeID){
 					float mp= totalHomePlayers-totalAwayPlayers;//manpower difference
 					int gd = homeGoals-awayGoals;//goaldifference
-					column+=",Gk_height,attack182,attack185,attack187,attack190,defend182,defend185,defend187,defend190, attack_avg_age, def_avg_age, mp_diff, gd";
-					values+=","+awaygk+","+home182+","+home185+","+home187 +","+ home190+","+away182+","+away185+","+away187+","+away190+","+ homeAverageAge +","+awayAverageAge+","+mp+","+gd;
+					
+					column+=",Gk_height,attack182,attack185,attack187,attack190,defend182,defend185,defend187,defend190, attack_avg_age, def_avg_age, mp_diff, gd, defend_id";
+					values+=","+awaygk+","+home182+","+home185+","+home187 +","+ home190+","+away182+","+away185+","+away187+","+away190+","+ homeAverageAge +","+awayAverageAge+","+mp+","+gd+","+awayID;
 
 				}
 				else{
 					float mp=totalAwayPlayers-totalHomePlayers; //manpower difference
 					int gd =awayGoals-homeGoals;
-					column+=",Gk_height,attack182,attack185,attack187,attack190,defend182,defend185,defend187,defend190, attack_avg_age, def_avg_age, mp_diff, gd";
-					values+=","+homegk+","+away182+","+away185+","+away187+","+away190+","+home182+","+home185+","+home187 +","+ home190+","+awayAverageAge+","+homeAverageAge+","+mp+","+gd;
+					column+=",Gk_height,attack182,attack185,attack187,attack190,defend182,defend185,defend187,defend190, attack_avg_age, def_avg_age, mp_diff, gd, defend_id";
+					values+=","+homegk+","+away182+","+away185+","+away187+","+away190+","+home182+","+home185+","+home187 +","+ home190+","+awayAverageAge+","+homeAverageAge+","+mp+","+gd+","+homeID;
 				}
 				ArrayList<Qualifier> takenlist=eventList.get(i+1).getQualifierList();
 				boolean xdone=false;
